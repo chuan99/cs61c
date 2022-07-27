f83650455fbda1e8f39c1fca75850c7f2704faff
“chuan99” <1163528774@qq.com>
2022-07-27 21:26:33 +0800

    finish lab2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,11 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+	/* YOUR CODE HERE */
+	uint16_t b0 = (*reg) & 1, b2 = (*reg) >> 2 & 1, b3 = (*reg) >> 3 & 1, b5 = (*reg) >> 5 & 1;
+	uint16_t b = b0 ^b2 ^b3 ^b5;
+	(*reg) >>= 1;
+	(*reg) = ((*reg) & 0x7fff) | b << 15;
 }
+
 
