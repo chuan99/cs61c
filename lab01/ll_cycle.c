4063af2c6d2f19bfb6e6c898b2a5bd9069ba3479
“chuan99” <1163528774@qq.com>
2022-07-27 21:23:55 +0800

    finish lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,25 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+	/* your code here */
+	if (head == NULL) {
+		return 0;
+	}
+	node *hare, *tortoise;
+	hare = tortoise = head;
+	while (hare != NULL) {
+		hare = hare->next;
+		if (hare == NULL) {
+			return 0;
+		}
+		hare = hare->next;
+		if (hare == NULL) {
+			return 0;
+		}
+		tortoise = tortoise->next;
+		if (tortoise == hare) {
+			return 1;
+		}
+	}
+	return 0;
 }
